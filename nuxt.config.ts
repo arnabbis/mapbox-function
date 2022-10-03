fcae3c135dd9af3e79c06809683174df9b20eafa
arnabbis <biswas.anuska.2004@gmail.com>
2022-10-03 15:32:06 +0530

    feat: integrating frontend with backend

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,5 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     ssr: false,
-    css: ["mapbox-gl/dist/mapbox-gl.css", "v-mapbox/dist/v-mapbox.css"]
+    css: ["mapbox-gl/dist/mapbox-gl.css", "v-mapbox/dist/v-mapbox.css", "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css",]
 })
